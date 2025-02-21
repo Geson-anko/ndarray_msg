347a5fd2ff4d4be857fac977e43dd9b07430d09a
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2025-02-21 01:51:42 +0000

    Add NDArray and conversion functions to module exports

--- src/ndarray_msg_utils/__init__.py
+++ src/ndarray_msg_utils/__init__.py
@@ -1,3 +1,13 @@
 from importlib import metadata
 
+from ndarray_msg.msg import NDArray
+
+from .conversion import from_ros_msg, to_ros_msg
+
 __version__ = metadata.version(__name__.replace("_", "-"))
+
+__all__ = [
+    "NDArray",
+    "to_ros_msg",
+    "from_ros_msg",
+]
