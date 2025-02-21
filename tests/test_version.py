aba224951750662b4bd0b4426026491524fb5088
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2025-02-21 01:17:45 +0000

    Add dependencies for numpy and tomli; update test to use tomli

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,5 +1,6 @@
-import tomllib
 from pathlib import Path
+
+import tomli
 
 import ndarray_msg_utils
 
@@ -9,5 +10,5 @@ PROJECT_ROOT = Path(__file__).parent.parent
 def test_version():
     assert isinstance(ndarray_msg_utils.__version__, str)
     with open(PROJECT_ROOT / "pyproject.toml", "rb") as f:
-        pyproject = tomllib.load(f)
+        pyproject = tomli.load(f)
     assert pyproject["project"]["version"] == ndarray_msg_utils.__version__
