5f165841b7cbb273b676aeeedea1952b2f0c2c06
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2025-02-23 03:14:43 +0000

    pyproject.toml: Rename package to 'ros2-ndarray-msg-utils' and update versioning details

--- src/ndarray_msg_utils/__init__.py
+++ src/ndarray_msg_utils/__init__.py
@@ -4,7 +4,7 @@ from ndarray_msg.msg import NDArray
 
 from .conversion import from_ros_msg, to_ros_msg
 
-__version__ = metadata.version(__name__.replace("_", "-"))
+__version__ = metadata.version("ros2-ndarray-msg-utils")
 
 __all__ = [
     "NDArray",
